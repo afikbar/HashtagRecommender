effdaa5c3fe640e34ccc95a37ff4db7989394531
Afik Bar <afikbar@gmail.com>
2020-09-10 21:00:27 +0300

    Try Fix

--- recommender.py
+++ recommender.py
@@ -1,21 +1,19 @@
 import pickle
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, g
 from flask.templating import render_template
 from werkzeug.utils import secure_filename
 from HashtagRecommenderModel import HashtagRecommender, join_file
 
 MYDIR = os.path.dirname(__file__)
 UPLOAD_FOLDER = os.path.join('static', 'uploads', '')
-
+hr_model = None
 app = Flask(__name__)
 app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 ALLOWED_EXTENSIONS = {'jpg', 'png', 'jpeg'}
-
-hr_model = None
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -28,9 +26,6 @@ def home():
 
 @app.route('/upload')
 def upload_form():
-    if hr_model is None:
-        join_file(source_dir='Model_Files', dest_file="model.pkl")
-        hr_model = pickle.load(open('model.pkl', 'rb'))
     image_name = request.args.get('image_name', default=False)
     return render_template('upload.html', image_name=image_name)
 
@@ -52,6 +47,7 @@ def upload_file():
                 filename = secure_filename(file.filename)
                 save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 file.save(save_path)
+                global hr_model
                 hashtags = hr_model.predict_hashtags(save_path, num_neighbors=20, num_predict=10).values[0]
                 flash(f"Recommended Hashtags: {', '.join(map(lambda x: '#'+x, hashtags))}", 'success')
                 return redirect(url_for('upload_form', image_name=filename))
@@ -71,10 +67,17 @@ def read_hashtag():
         input_hashtags = request.form['hashtag'].split(',')
         filename = request.args.get('image_name')
         save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        global hr_model
         hashtags = hr_model.predict_hashtags(save_path, num_neighbors=20, num_predict=10, selected_hashtags=input_hashtags).values[0]
         flash(f"Recommended Hashtags: {', '.join(map(lambda x: '#'+x, hashtags))}", 'success')
         return redirect(request.environ['HTTP_REFERER'])
 
+
+@app.before_first_request
+def before_first_request_func():
+    join_file(source_dir='Model_Files', dest_file="model.pkl")
+    global hr_model
+    hr_model = pickle.load(open('model.pkl', 'rb'))
 
 if __name__ == '__main__':
     app.run()
